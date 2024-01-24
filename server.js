ae3b35f80e32c123c28c0478c6f2856f0ae3e894
Pedro Elias <122770043+edevPedro@users.noreply.github.com>
2024-01-24 13:44:32 -0300

    minor changes

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { createServer} from 'node:http';
+import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
 
-const app =  express();
+const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
@@ -13,65 +13,60 @@ app.use('/dist', express.static('assets'));
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+function defineStatus(n) {
+  const status = {
+    1: 'Esperando acesso',
+    2: 'Timer rodando',
+    3: 'Pronto para iniciar'
+  }
+  console.log(status[n])
+  io.emit('status', status[n])
+}
 
-app.get('/', (req, res) =>{
-  res.sendFile(join(__dirname, './dist/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, '/dist/index.html'));
 })
+
 
 io.on('connection', (socket) => {
   console.log('usuário conectado');
+  socket.on('login', (username) => {
+    socket.on('status', (status) => {
+      if (status == 'Esperando acesso' && username == 'admin') {
+        defineStatus(3);
+      } else if (status == 'Pronto para iniciar' && username == 'admin') {
 
+      }
+      else {
+        console.log('status inválido\n' + status)
+      }
+    });
+    socket.on('start', _ => {
+      socket.on('statusReturn', (statusReturn) => {
+        if (username == 'admin' && statusReturn === 'Pronto para iniciar') {
+          console.log('timer iniciado');
+          let ocounter = 14;
+          const counter = setInterval(() => {
+            defineStatus(2);
+            io.emit('timer', ocounter.toString());
+            ocounter--;
+            if (ocounter < 1) {
+              clearInterval(counter);
+              io.emit('timer', ocounter = 15);
+              defineStatus(1);
+            }
+          }, 1000);
+        } else {
+          console.log("farei nada não")
+        }
+      })
+    })
+  });
   socket.on('disconnect', () => {
     console.log('usuário desconectado');
   });
 });
 
-io.on('connection', (socket) => {
-  socket.on('login', (username) => {
-    if (username === 'admin'){
-      socket.on('status', (status) => {
-        if(status === 'Pronto para iniciar'){
-          socket.on('start', () =>{
-              console.log('timer iniciado');
-              let ocounter = 14;
-              const counter = setInterval(() => {
-                io.emit('timer', ocounter.toString());
-                ocounter--;
-                if (ocounter < 0){
-                  clearInterval(counter);
-                  io.emit('timer', ocounter = 15);
-                }
-              }, 1000);
-            });
-            io.emit('status', 'Esperando acesso.');
-          }else{
-            io.emit('status', 'Pronto para iniciar');
-          }
-          console.log('mudança de status');
-        })
-    }
-  });
-})
-
-
-// io.on('connection', (socket) => {
-//   socket.on('login', (username) => {
-//     if (username === 'admin'){
-//       socket.join('admin');
-//       socket.on('start', () =>{
-//         console.log('timer iniciado');
-//         let ocounter = 14;
-//         const counter = setInterval(() => {
-//         io.emit('timer', ocounter.toString());
-//         ocounter--;
-//         if (ocounter < 0){
-//           clearInterval(counter);
-//         }
-//           }, 1000);
-//       });
-//     }
-//   });
-// })
 
 server.listen(3000, () => {
   console.log('server rodando na famosa porta 3000');
--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,55 @@
-import  { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import telekLogo from './assets/logo-telek.png';
 import io from 'socket.io-client';
 
 const socket = io('http://localhost:3000');
 
-function Timer(){
+function Timer() {
   const [inputValue, setInputValue] = useState('');
-  const [status, setStatus] = useState('Esperando acesso.');
+  const [status, setStatus] = useState('Esperando acesso');
   const [time, setTime] = useState(15);
+  const [wordColor, setWordColor] = useState('red')
 
   useEffect(() => {
     socket.on('status', (status) => {
       setStatus(status);
+      if (status == 'Pronto para iniciar') {
+        setWordColor('#1A9145');
+      } else if (status == 'Timer rodando') {
+        setWordColor('#1F85D9')
+      } else {
+        setWordColor('red');
+      }
     })
     socket.on('timer', (time) => {
       setTime(time);
     })
-  
+
   }, [])
-  
+
   const buttonYClick = () => {
-    socket.emit('login', inputValue);
-    socket.emit('start');
+    if (inputValue == 'admin') {
+      socket.emit('login', inputValue);
+      socket.emit('statusReturn', (status))
+      console.log('statusReturn enviado')
+      socket.emit('start');
+    } else (console.log('nada a startar'))
   }
 
   const changeStatus = () => {
-    socket.emit('login', inputValue);
-    socket.emit('status', status);
+    if (inputValue == 'admin') {
+      socket.emit('login', inputValue);
+      socket.emit('status', status);
+    } else (console.log('nada a fazer'))
   }
 
 
-  return(
-      <div>
-     <label htmlFor='username'>Username: </label>
-     <input type='text' id='username' value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+  return (
+    <div>
+      <label htmlFor='username'>Username: </label>
+      <input type='text' id='username' value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
       <br />
-      <p><span id='status-color' >Status</span>: <span id="status">{status}</span></p>
+      <p><span id='wordStatus' style={{ color: wordColor }} onChange={(e) => setWordColor()}>Status</span>: <span id="status">{status}</span></p>
       <p>Tempo restante: <span id="timer">{time}</span> segundos</p>
       <div className="pBtn">
         <button id="start" onClick={buttonYClick} >Iniciar Timer</button>
@@ -47,14 +61,13 @@ function Timer(){
 
 
 function App() {
-  
-  return (
 
+  return (
     <>
       <img src={telekLogo} alt="Logo telek" />
       <h1>Controle via <span className='blue'>Socket</span></h1>
-      <Timer/>
+      <Timer />
     </>
-    );
+  );
 }
 export default App;
